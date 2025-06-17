dac8a73882a342270987c9dcfbac6f901e435283
Peter Kyale <kyalepeter2000@gmail.com>
2025-06-17 12:21:14 +0300

    updated the add_swap contract deploy script

--- scripts/06_add_swapDeploy.js
+++ scripts/06_add_swapDeploy.js
@@ -10,13 +10,25 @@ const ABYATKN_ADDRESS = process.env.APP_ABYATKN_ADDRESS;
 const USDC_ADDRESS = process.env.APP_USDC_ADDRESS;
 const WRAPPED_BITCOIN_ADDRESS = process.env.APP_WRAPPED_BITCOIN_ADDRESS;
 
+console.log('ABYATKN_USDC_500=', `'${ABYATKN_USDC_500}'`);
+console.log('ABYATKN_ADDRESS=', `'${ABYATKN_ADDRESS}'`);
+console.log('USDC_ADDRESS=', `'${USDC_ADDRESS}'`);
 
 // Uniswap contract addresses
-const SWAP_ROUTER_ADDRESS = process.env.SWAP_ROUTER_ADDRESS;
-const POSITION_MANAGER_ADDRESS = process.env.POSITION_MANAGER_ADDRESS;
+const SWAP_ROUTER_ADDRESS = process.env.APP_SWAP_ROUTER_ADDRESS;
+const POSITION_MANAGER_ADDRESS = process.env.APP_POSITION_MANAGER_ADDRESS;
+
+console.log('SWAP_ROUTER_ADDRESS=', `'${SWAP_ROUTER_ADDRESS}'`);
+console.log('POSITION_MANAGER_ADDRESS=', `'${POSITION_MANAGER_ADDRESS}'`);
+
 
 async function main() {
     const [owner, addr1] = await ethers.getSigners();
+
+    if (!SWAP_ROUTER_ADDRESS || !POSITION_MANAGER_ADDRESS || !ABYATKN_ADDRESS || !USDC_ADDRESS) {
+        throw new Error("Missing environment variables!");
+    }
+
 
     const SwapDeploy = await ethers.getContractFactory("Add_Swap_Contract", owner);
 
